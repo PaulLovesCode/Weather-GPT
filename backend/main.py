ecbb38bf30017b758229436f2282038d5cc2eab5
PaulLovesCode <paulsudipoff@gmail.com>
2026-09-25 02:44:26 +0530

    cors updation

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=os.getenv(
         "CORS_ORIGINS",
-        "http://localhost:3000,http://127.0.0.1:3000",
+        "http://localhost:3000,http://127.0.0.1:3000,https://atmosphereai.vercel.app/",
     ).split(","),
     allow_credentials=True,
     allow_methods=["*"],
